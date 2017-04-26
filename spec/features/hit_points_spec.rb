121c2de483bf3eb5a9ba8d8e43b6aca77d68e84a
Cristina Iannone <iannonecristina@gmail.com>
2017-04-26 17:48:44 +0100

    Adds player_2_name to play.erb

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,9 @@
 feature 'Hit points' do
   scenario 'view hit points of P2' do
-    visit '/play'
-    expect(page).to have_content("P2 - 30HP")
+    visit('/')
+      fill_in :player_1_name, with: 'Dave'
+      fill_in :player_2_name, with: 'Mittens'
+      click_button 'Submit'
+      expect(page).to have_content 'Mittens: 30HP'
   end
 end
