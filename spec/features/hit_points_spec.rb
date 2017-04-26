f537c549b67a9905469ee7c73d01abddfeb09464
Cristina Iannone <iannonecristina@gmail.com>
2017-04-26 17:56:13 +0100

    Refactors spec files using web_helpers.rb logic

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'Hit points' do
   scenario 'view hit points of P2' do
-    visit('/')
-      fill_in :player_1_name, with: 'Dave'
-      fill_in :player_2_name, with: 'Mittens'
-      click_button 'Submit'
+    sign_in_and_play
       expect(page).to have_content 'Mittens: 30HP'
   end
 end
