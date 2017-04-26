9c9cd5deb08d71e6dca0e610f1659ac752757ee2
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-26 16:20:16 +0100

    start implementing sessions

--- app.rb
+++ app.rb
@@ -1,16 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
+    "value = " << session[:value].inspect
   end
 
-  post '/names' do
+  get '/:value' do
+    session['value'] = params['value']
+  end
+
+  post '/play:value' do
     p params
     @player_1_name = params[:player_1_name]
     @player_2_name = params[:player_2_name]
-    erb :names
+    erb :play
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
   end
 
   run! if app_file == $0
