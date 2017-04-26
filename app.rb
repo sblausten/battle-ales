c0954bb28c1a56999dcf3ea125a2748541b101ab
Cristina Iannone <iannonecristina@gmail.com>
2017-04-26 15:27:08 +0100

    Adds refactored testing_names_spec and app.rb

--- app.rb
+++ app.rb
@@ -2,14 +2,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  get '/' do
-    "Testing infrastructure working!"
-  end
+  #get '/' do
+  #  "Testing infrastructure working!"
+  #end
 
   get '/' do
+    #p params
+    erb :index
+    #params = [:name]
+  end
+
+  post '/names' do
     p params
-    erb(:index)
-    params = [:name]
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :names
   end
 
   run! if app_file == $0
--- spec/features/testing_names_spec.rb
+++ spec/features/testing_names_spec.rb
@@ -1,6 +1,9 @@
 feature 'Testing names' do
   scenario 'players fill in names' do
     visit '/'
-    expect(page).to have_xpath("//input")
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button('Submit')
+    expect(page).to have_content('Dave vs. Mittens')
   end
 end
